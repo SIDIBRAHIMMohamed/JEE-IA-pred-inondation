77462936277a660c2c33ac1ff1fd5b3b30de4e2d
Aziz Jiddou <21085@esp.mr>
2023-02-23 09:09:13 +0000

    initialisation projet front-back

---
+++ back-end/src/test/java/com/esp/irt/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esp.irt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
