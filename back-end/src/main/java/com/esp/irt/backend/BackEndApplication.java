40d89524b800c5be4c032d7237f6e69b4d2c5a94
Aziz Jiddou <21085@esp.mr>
2023-05-09 09:58:06 +0000

    ajout gestion des logs

--- back-end/src/main/java/com/esp/irt/backend/BackEndApplication.java
+++ back-end/src/main/java/com/esp/irt/backend/BackEndApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication(exclude = BatchAutoConfiguration.class)
 public class BackEndApplication {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(BackEndApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+
+		LOGGER.info("This is information");
+		LOGGER.debug("This is debug");
+		LOGGER.error("This is error");
 	}
 
 }
